36a630f846df1e9f7415c22828f3e227e99df987
Greve2001 <frederik.greve@gmail.com>
2023-04-11 08:50:33 +0200

    Added screenlock utility

---
+++ .config/scripts/lockscreen.sh
@@ -0,0 +1,14 @@
+#!/bin/env bash
+
+path=$HOME/Images/Lockscreen/tmpbg
+
+# Remove temp bgs
+rm -f "$path*.png"
+
+# Take screenshot
+grim "$path.png"
+
+# Use gaussion blur
+ffmpeg -i "$path.png" -filter:v "gblur=sigma=20" -frames:v 1 "$path%03d.png" > /dev/null 2>&1 
+
+swaylock -c '1b2021' -i "$path""001.png"
