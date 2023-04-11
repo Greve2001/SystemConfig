a5281d769f3395373ded4a8a768b59cb622501bf
Greve2001 <frederik.greve@gmail.com>
2023-04-11 09:23:09 +0200

    Customized swaylock a bit

--- .config/swaylock/lockscreen.sh
+++ .config/swaylock/lockscreen.sh
@@ -11,4 +11,4 @@ grim "$path.png"
 # Use gaussion blur
 ffmpeg -i "$path.png" -filter:v "gblur=sigma=20" -frames:v 1 "$path%03d.png" > /dev/null 2>&1 
 
-swaylock -c '1b2021' -i "$path""001.png"
+swaylock -C "$HOME/.config/swaylock/config" -c '1b2021' -i "$path""001.png"
